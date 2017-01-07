f3b3151bbc86fa62eec9b120a81eaeddfd646ab4
Zargon Karushi <Zargon Karushi>
2017-01-07 18:48:47 +0600

    finished project and added gitignore

--- bp.cpp
+++ bp.cpp
@@ -6,6 +6,8 @@
 #define VI vector < int >
 #define read(a) freopen(a, "r", stdin)
 #define write(a) freopen(a, "w", stdtout)
+#define clr(a) memset(a, 0, sizeof(a))
+#define VE vector < Edge >
 
 using namespace std;
 #define VP vector < Pair >
@@ -13,7 +15,8 @@ using namespace std;
 map < int, int > M;// map value starts from 1, not 0 (for distinguishing)
 VI G[MAX];// vector that defines the graph
 bool active[MAX];// true if a function (variable) node is active
-bool flag[MAX][MAX];// flag[u][v] is true if u has sent data to v
+bool flag[MAX][MAX];// flag[u][v] is true if u has sent data to v already i.e. edge u to v
+					// has been active
 int comp[MAX][MAX], trans[MAX][MAX];// asymetric matrix containing costs
 char str[MAX];// input machines
 int nodes[MAX];// nodes by index
@@ -59,7 +62,8 @@ struct Edge {
 	}
 	
 	void print(){
-		cout << u << " " << v << " " << cost[u][v] << endl;
+		cout << "data sending from "<< u << " to " << v << ". computation cost " << comp[u][v] 
+		<< " transmission cost " << trans[u][v]<< endl;
 	}
 };
 
@@ -107,6 +111,11 @@ void process_inputs(){
 		Machines[num_mac] = tmp;
 		num_mac++;
 	}
+}
+
+int total_cost(Edge e){
+	// may be updated because of the simulation, so calculate every time
+	return (comp[e.u][e.v]+trans[e.u][e.v]);
 }
 
 // debug prints
@@ -172,40 +181,107 @@ bool same_mac(int u, int v){
 }
 
 void reset(){
-	memset(flag, 0, MAX);
+	clr(flag);
 }
 
 int num_act = 0;
+int mac_vis[MAX];// for each iteration, it is reset. if mac_vis[u] is true,
+				 // then this machine has been visited already.
+
+VE tmp;
+int total = 0;
 
 void propagate(){
-	int cnt = 1;
-	priority_queue < Edge > Q;
-	while (!Q.empty())	Q.pop();
-	//while (cnt <= 1){
+	clr(flag);
+	// first test for just one iteration
+	while (true){
+		clr(mac_vis);
+		tmp.clear();
+		cout << "Time " << total << ": " << endl;
 		for (int u = 1; u < val; u++){
 			int sz = G[u].size();
+			bool is_vis = mac_vis[idx_mac[u]];
+			//cout << "In node " << u << endl;
+			// parent machine of node u. if visited, ignore all the edges that go from u
+			//if (is_vis)	continue;
+			
 			for (int i = 0; i < sz; i++){
 				int v = G[u][i];
 				Edge e = Edge(u, v);
+				// if this edge is visited already, then true
+				if (flag[u][v])	continue;
+				//cout << u << " " << v << " " << idx_mac[u] << " " << flag[u][v] << endl;
 				bool fl = e.is_active();
-				//cout << u << " " << v << " " << fl << endl;
-				if (fl)	Q.push(e);
+				if (fl){
+					flag[u][v] = true;
+					mac_vis[idx_mac[u]] = true;
+					tmp.pb(e);
+				}
 			}
 		}
-		Edge e = Q.top();
-		e.print();
-		
-		//cnt++;
-	//}
+		// no more active edge to be found
+		if (tmp.size() == 0){
+			cout << "Propagation finished" << endl;
+			break;
+		}
+		int min_cost = INF, idx = 0, i_val = 0;
+		// idx is the machine id with minimum total cost
+		// find the minimum total cost
+		// change to some other cost in total_cost function if necessary
+		// iterate through all active nodes
+		for (int i = 0; i < tmp.size(); i++){
+			//tmp[i].print();
+			//cout << "node "<< tmp[i].u << " sends data to " << tmp[i].v << endl;
+			// total cost of the current active node
+			int val = total_cost(tmp[i]);
+			if (min_cost > val){
+				min_cost = val;
+				i_val = i;
+				idx = idx_mac[tmp[i].u];
+			}
+		}
+		total += min_cost;
+		for (int i = 0; i < tmp.size(); i++){
+			if (i_val == i){
+				cout << "Minimum cost: Data sent from "<< tmp[i].u << " to " << tmp[i].v << endl;
+				continue;// the node with min_cost. nothing to do
+			}
+			int u = tmp[i].u, v = tmp[i].v;
+			// if on the same machine, reduce the computation time but don't transmit
+			if (idx_mac[u]  == idx){
+				comp[u][v] = max(0, comp[u][v]-min_cost);
+				cout << "Data computed for edge "<< u << " to " << v << ", costs: " << comp[u][v]<< 
+				" "<< trans[u][v] <<endl;
+			}
+			// if on different machine, reduce comp cost first, then transmit since it can be sent
+			else {
+				// if comp cost alone is bigger than min_cost, transmit cost can not be reduced
+				if (comp[u][v] >= min_cost){
+					comp[u][v] -= min_cost;
+				}
+				// otherwise, both can be reduced, and comp cost is now 0
+				else {
+					int val = min_cost-comp[u][v];
+					comp[u][v] = 0;
+					trans[u][v] -= val;
+					trans[u][v] = max(0, trans[u][v]);// in case it is less than 0
+				}
+				cout << "Data sent from "<< u << " to " << v << ", costs: " << comp[u][v] << " "<< 
+				trans[u][v] << endl;
+			}
+		}
+		cout <<  endl;
+	}
 	
 }
 
 
 int main(){
-	read("input.txt");
+	read("input2.txt");
 	val = 1;
 	process_inputs();
 	propagate();
+	//cout << total << endl;
 	//test_queue();
 	
 	return 0;
